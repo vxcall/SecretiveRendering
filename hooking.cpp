4e8bc779a4b7ef44da06ac01c3cf3ea84fc85075
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-02-22 21:07:34 +0800

    Sorted out the code structure
    I made a juicy little function Hook() in hooking.h which basically is a wrapper of Creating hook.

--- hooking.cpp
+++ hooking.cpp
@@ -5,14 +5,15 @@
 #include "findpattern.h"
 #include "debugMessage.h"
 
+// Defining custom types of target function respectively.
 using tPresent = HRESULT(STDMETHODCALLTYPE*) (IDirect3DDevice9*, const RECT*, const RECT*, HWND, const RGNDATA*);
 using tReset = HRESULT (STDMETHODCALLTYPE*) (IDirect3DDevice9*, D3DPRESENT_PARAMETERS*);
 
+// You need these nulled function both to use in the hookFunc, and to pass MinHook.
 tPresent oPresent = nullptr;
 tReset oReset = nullptr;
 
 HRESULT STDMETHODCALLTYPE hkPresent(IDirect3DDevice9* thisptr, const RECT* src, const RECT* dest, HWND wnd_override, const RGNDATA* dirty_region) {
-    std::cout << "I'm in Present" << std::endl;
     D3DCOLOR color = D3DCOLOR_XRGB(255, 0, 0);
     D3DRECT position = {100, 100, 200, 200};
 
@@ -21,7 +22,6 @@ HRESULT STDMETHODCALLTYPE hkPresent(IDirect3DDevice9* thisptr, const RECT* src, 
 }
 
 HRESULT STDMETHODCALLTYPE hkReset(IDirect3DDevice9* thisptr, D3DPRESENT_PARAMETERS* params) {
-    std::cout << "I'm in Reset" << std::endl;
     return oReset(thisptr, params);
 }
 
@@ -31,18 +31,14 @@ void hooks::initialize() {
         {
             throw std::exception("MH_Initialize failed!");
         }
+        // Getting the original address of Present and Reset function
         uintptr_t originalPresentAddress = FindPattern("gameoverlayrenderer.dll", "68 ? ? ? ? 68 ? ? ? ? FF 76 44") + 0x1;
         uintptr_t originalResetAddress = FindPattern("gameoverlayrenderer.dll", "FF 15 ? ? ? ? 8B F8 85 FF 78 18 85 F6 74 14 FF 75 E0 8B CE 53 FF 75 EC E8 ? ? ? ?") + 2;
         LOGHEX("OriginalPresentAddress", originalPresentAddress)
         LOGHEX("OriginalResetAddress", originalResetAddress)
-        if (MH_CreateHook(**reinterpret_cast<void***>(originalPresentAddress), &hkPresent, reinterpret_cast<LPVOID*>(&oPresent)) != MH_OK)
-        {
-            throw std::exception("Present hooking failed!");
-        }
-        if (MH_EnableHook(**reinterpret_cast<void***>(originalPresentAddress)) != MH_OK)
-        {
-            throw std::exception("Enabling Present hook failed!");
-        }
+
+        Hook(**reinterpret_cast<void***>(originalPresentAddress), &hkPresent, &oPresent); // Actual hooking. See Hook function in the header file.
+        Hook(**reinterpret_cast<void***>(originalResetAddress), &hkReset, &oReset);
 
     } catch (const std::exception &ex) {
         MessageBoxA(nullptr, ex.what(), "Error", 0);
--- hooking.h
+++ hooking.h
@@ -1,8 +1,19 @@
 #pragma once
 #include "MinHook.h"
 #include <d3d9.h>
-
+#include <iostream>
 
 namespace hooks {
+    template<typename T>
+    void Hook(void* pTarget, void* pDetour, T** ppOriginal)
+    {
+        MH_STATUS creationStatus = MH_CreateHook(pTarget, pDetour, reinterpret_cast<LPVOID*>(ppOriginal));
+        if (creationStatus != MH_OK)
+            std::cout << "MH_CreateHook() failed" << std::endl;
+        MH_STATUS enableStatus = MH_EnableHook(pTarget);
+        if (enableStatus != MH_OK)
+            std::cout << "MH_EnableHook() failed" << std::endl;
+    }
+
     void initialize();
 }
